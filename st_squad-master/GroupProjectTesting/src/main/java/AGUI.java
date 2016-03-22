237eeb7171982661ff6dfa224d34525873338a54
talltslife <talltslife@gmail.com>
2016-03-22 08:44:04 -0400

    work on gui

--- st_squad-master/GroupProjectTesting/src/main/java/AGUI.java
+++ st_squad-master/GroupProjectTesting/src/main/java/AGUI.java
@@ -3,28 +3,38 @@ package main.java;
 
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+
 
 public class AGUI extends JFrame {
 	
 	//private AddressBookController controller;
+	private JInternalFrame front;
+	
+	private JInternalFrame back;
 	private AddressBook addressBook;
 	private AbstractListModel nameListModel;
 	private JList nameList;
-	private JButton addButton;
 	private JButton editButton;
 	private JButton deleteButton;
 	private JButton sortByNameButton;
 	private JButton sortByZipButton;
-	private JMenuItem newItem;
-	private JMenuItem openItem;
-	private JMenuItem saveItem;
-	private JMenuItem saveAsItem;
-	private JMenuItem printItem;
-	private JMenuItem quitItem;
+	private JButton newItem;
+	private JButton openItem;
+	private JButton saveItem;
+	private JButton saveAsItem;
+	private JButton printItem;
+	private JButton quitItem;
 	private JPanel ButtonConatainer;
 	private Container ListContainer;
 	private Container FieldContainer;
@@ -44,6 +54,30 @@ public class AGUI extends JFrame {
 	private JTextField cityField;
 	private GridLayout gridLayout2;
 	private JPanel fieldConatiner;
+	private JPanel searchPanel;
+	private JRadioButton searchLast;
+	private JRadioButton searchFirst;
+	private JRadioButton searchZip;
+	private JRadioButton searchStreet;
+	private JRadioButton searchPhone;
+	private JRadioButton searchCity;
+	private ButtonGroup radioGroup;
+	private JTextField searchTextField;
+	private JButton searchButton;
+	private GridLayout serchLayout;
+	private JPanel radioPanel;
+	
+	
+	private JButton addButton;
+	private JList bookList;
+	private JTextField newNameField;
+	private GridLayout bottomLayout;
+	private JPanel panel;
+	private JPanel bottomPanel;
+	
+	private JPanel cards;
+	
+	JDesktopPane desk;
 	
 	
 	
@@ -52,7 +86,29 @@ public class AGUI extends JFrame {
 		super("Address Book");	
 		
 		//Getting Address Books to Display
-		//String[] peopleObjects = {"FRed", "Tim", "Doug", "Sally", "mom"};
+		String[] peopleObjects = {"FRed", "Tim", "Doug", "Sally", "mom"};
+		
+		
+		
+		JInternalFrame front = new JInternalFrame("front",true,true,true,true);
+		desk = new JDesktopPane();
+		add(desk);
+		front.setLayout(new BorderLayout());
+		
+		JInternalFrame back = new JInternalFrame();
+		
+		desk.add(front);
+		desk.add(back);
+		
+		front.setSize(500,500);
+		
+		
+		front.setVisible(true);
+		back.setVisible(false);
+		
+		
+		
+		
 		
 		/*ArrayList<String> books = null;
 		try {
@@ -86,43 +142,41 @@ public class AGUI extends JFrame {
 		//If exists the address will be passed back and fill in the peopleObjects
 		ArrayList<Person> people = null;
 		
-		AddressBook b = new AddressBook("TONYSBOOK2");
-		people = b.getAddresses();
+	//	AddressBook b = new AddressBook("TONYSBOOK2");
+//		people = b.getAddresses();
 		
-		String[] peopleObjects = new String[people.size()];
-		for(int i = 0; i < people.size(); i++){
-			peopleObjects[i] = people.get(i).getFirstName();
-		}
+	//	String[] peopleObjects = new String[people.size()];
+	//	for(int i = 0; i < people.size(); i++){
+	//		peopleObjects[i] = people.get(i).getFirstName();
+	//	}
 		//peopleObjects = books.toArray(peopleObjects);
 		
 		
 		
-		gridLayout = new GridLayout(3,3);
-		addButton = new JButton("add");
+		gridLayout = new GridLayout(2,3);
 		editButton = new JButton("edit");
 		deleteButton= new JButton("delete");
 		sortByNameButton = new JButton("sort by name");
 		sortByZipButton = new JButton("sort by zip");
-		printItem = new JMenuItem("print");
-		newItem = new JMenuItem("new");
-		saveItem = new JMenuItem("save");
-		saveAsItem = new JMenuItem("saveas/update");
+		printItem = new JButton("print");
+		newItem = new JButton("new");
+		saveItem = new JButton("save");
+		saveAsItem = new JButton("saveas/update");
 		ButtonConatainer = new JPanel();
 		ButtonConatainer.setLayout(gridLayout);
-		add(ButtonConatainer, BorderLayout.SOUTH);
-		ButtonConatainer.add(addButton);
-		ButtonConatainer.add(editButton);
+		back.add(ButtonConatainer, BorderLayout.SOUTH);
+	
+		ButtonConatainer.add(newItem);
+		ButtonConatainer.add(saveItem);
 		ButtonConatainer.add(deleteButton);
 		ButtonConatainer.add(sortByNameButton);
 		ButtonConatainer.add(sortByZipButton);
 		ButtonConatainer.add(printItem);
-		ButtonConatainer.add(newItem);
-		ButtonConatainer.add(saveItem);
-		ButtonConatainer.add(saveAsItem);
+		
 		
 		JList nameList = new JList(peopleObjects);
 		nameList.setVisibleRowCount(10);
-		add(nameList, BorderLayout.WEST);
+		back.add(nameList, BorderLayout.WEST);
 		
 		firstName = new JTextField("first name");
 		lastName= new JTextField("last name");
@@ -144,7 +198,7 @@ public class AGUI extends JFrame {
 		gridLayout2 = new GridLayout(6,2);
 		fieldConatiner = new JPanel();
 		fieldConatiner.setLayout(gridLayout2);
-		add(fieldConatiner, BorderLayout.EAST);
+		back.add(fieldConatiner, BorderLayout.EAST);
 		fieldConatiner.add(firstName);
 		fieldConatiner.add(lastName);
 		fieldConatiner.add(street);
@@ -158,9 +212,137 @@ public class AGUI extends JFrame {
 		fieldConatiner.add(phone);
 		fieldConatiner.add(phoneField);
 		
+		radioGroup = new ButtonGroup();
 		
+		searchPhone = new JRadioButton("phone",false);
+		searchLast = new JRadioButton("last name", false);
+		searchFirst = new JRadioButton("first name", true);
+		searchZip = new JRadioButton("zip",false);
+		searchCity = new JRadioButton("city", false);
+		searchStreet = new JRadioButton("street", false);
+		searchButton = new JButton("search");
+		radioGroup.add(searchFirst);
+		radioGroup.add(searchLast);
+		radioGroup.add(searchZip);
+		radioGroup.add(searchCity);
+		radioGroup.add(searchStreet);
+		radioGroup.add(searchPhone);
+	
+		searchPanel = new JPanel();
+		radioPanel = new JPanel();
+		serchLayout = new GridLayout(2,1);
+		searchPanel.setLayout(serchLayout);
+		radioPanel.add(searchFirst);
+		radioPanel.add(searchLast);
+		radioPanel.add(searchZip);
+		radioPanel.add(searchCity);
+		radioPanel.add(searchStreet);
+		radioPanel.add(searchPhone);
+		radioPanel.add(searchButton);
+		
+		searchTextField = new JTextField(10);
+		
+		
+		
+		searchPanel.add(radioPanel);
+		searchPanel.add(searchTextField);
+		back.add(searchPanel, BorderLayout.NORTH);
+		
+		searchFirst.addItemListener(new RadioButtonHandler());
+		searchLast.addItemListener(new RadioButtonHandler());
+		searchZip.addItemListener(new RadioButtonHandler());
+		searchCity.addItemListener(new RadioButtonHandler());
+		searchStreet.addItemListener(new RadioButtonHandler());
+		searchPhone.addItemListener(new RadioButtonHandler());
+		searchButton.addItemListener(new RadioButtonHandler());
+		
+		searchButton.addActionListener(new ButtonHandler());
+
+		
+		deleteButton.addActionListener(new ButtonHandler());
+		sortByNameButton.addActionListener(new ButtonHandler());
+		sortByZipButton.addActionListener(new ButtonHandler());
+		newItem.addActionListener(new ButtonHandler());
+		saveItem.addActionListener(new ButtonHandler());
+		printItem.addActionListener(new ButtonHandler());
+		
+		
+		
+		
+		String[] books = { "james", "Ted",  "Coolio", "wierd al", "Tim"};
+		
+		addButton = new JButton("add");
+		bookList = new JList(books);
+		newNameField = new JTextField(15);
+		panel = new JPanel();
+		//add(panel, BorderLayout.CENTER);
+		bottomPanel = new JPanel();
+		
+		
+		
+		bottomLayout = new GridLayout(1,2);
+		bottomPanel.setLayout(bottomLayout);
+		
+		bottomPanel.add(newNameField);
+		bottomPanel.add(addButton);
+		front.add(bottomPanel, BorderLayout.SOUTH);
+		front.add(bookList, BorderLayout.CENTER);
+		ButtonHandler handler = new ButtonHandler();
+		addButton.addActionListener(handler);
+		
+		
+		
+
+	
+		
+	}
+	private class RadioButtonHandler implements ItemListener{
+
+		public void itemStateChanged(ItemEvent e) {
+		//	searchTextField.searchx
+			
+		}
 		
 	}
 	
+	private class ButtonHandler implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==addButton){
+				front.setVisible(false);
+				back.setVisible(true);
+			}
+			
+		}
+		
+	}
+	
+	private class textFieldListener implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+		//	if(e == searchTextField)
+			
+		}
+		
+	}
+	
+	private class listListener implements ListSelectionListener{
+
+		public void valueChanged(ListSelectionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public static final void main(String[] args){
+		
+	}
 
 }
+
+
+
+
+
+
--- st_squad-master/GroupProjectTesting/src/main/java/GuiRunner.java
+++ st_squad-master/GroupProjectTesting/src/main/java/GuiRunner.java
@@ -6,7 +6,9 @@ public class GuiRunner {
 	public static void main(String[] agrs){
 		AGUI gui = new AGUI();
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setSize(500,500);
+		gui.setSize(550,550);
 		gui.setVisible(true);
+		
+		
 	}
 }
